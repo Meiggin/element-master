dc2cce084227cb05cfdf3493eed43b301c2593c5
Meiggin <781571871@qq.com>
2019-03-13 09:55:52 +0800

    api统一管理

---
+++ src/request/api/index.js
@@ -0,0 +1,23 @@
+/**
+ * api接口统一管理
+ */
+import request from './../http'
+
+
+// export function changeStatus(data) {
+//     const params = Object.assign({}, data)
+//     return request({
+//         url: '/biz/api/outTickets',
+//         method: 'post',
+//         data: params
+//     })
+// }
+
+// export function changeMoreOdds(data) {
+//     const params = Object.assign({}, data)
+//     return request({
+//         url: '/biz/api/changeBettingContentByIds',
+//         method: 'get',
+//         params
+//     })
+// }
